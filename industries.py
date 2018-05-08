a6b688d01e755447d77be71c18cee89ec6d14fdb
sohi <SteveOhi@users.noreply.github.com>
2018-05-08 12:42:31 -0400

    Add files via upload

--- industries.py
+++ industries.py
@@ -1,159 +1,213 @@
+#!/usr/bin/python3.5
+
+# This program is used to match urls with Industry names from the finance.yahoo.com/Industries page as follows:
+# The program first obtains the symbol and url using beautiful soup
+# It then obtains the symbol and Industry name using dataframes
+# Then using the symbol as the common field from each table, it then matches the Industry name with the url
+# Finally, it stores the industry name and url in a table which can be referenced at a later date to see if there has been
+# any changes in the urls or industry names
+
 import bs4 as bs
 import urllib.request
 import pandas as pd
+import lxml
+import html5lib
 import fileinput
 import subprocess
 import os
 import logging
 import time
 import datetime
+import psycopg2
+import re
+import csv
+import numpy as np
+import io
 
-# This program checks to see if there was a spike in volume in one of the industries in the NYSE.  
+# This program checks to see if there was a spike in volume in one of the industries in the NYSE.
 # December 1, 2017 Steve Ohi
 
-# logging.basicConfig(format='%(asctime)s %(message)s', filename='logfile.txt',level=logging.DEBUG)
+# logging.basicConfig(format='%(asctime)s %(message)s',filename='logfile.txt',level=logging.DEBUG)
 # logging.debug('Debug Level 1')
 # logging.info('Info Level 1')
 # logging.warning('Warning Level 1')
 
 # scrape_industries() scrapes the list of industries from https://finance.yahoo.com/industries/
 def scrape_industries():
-  ''' Start by pulling the industries table from Yahoo Finance into a pandas dataframe table '''
-  dfs = pd.read_html('https://finance.yahoo.com/industries/',header=0)
+# Start by scraping the industries table from Yahoo Finance into a pandas dataframe table
+  ind_data = pd.read_html('https://finance.yahoo.com/industries/', header=0)
   pd.set_option('display.max_rows', 500)
-  f = open('industries_scraped.txt', 'w')
-  print(dfs, end="", file=f, flush=False)
-  f.close()
-  # subprocess.call removes the first square bracket from the industries_scraped.txt and print the output to industries_current.txt
-  subprocess.call("./removeSB.sh", shell=True)
+# Save the dataframe to a csv (see http://www.pythonforbeginners.com/systems-programming/using-the-csv-module-in-python/)
+  ind_data[0].to_csv('./tmp/ind_data', header=None, index=False, sep=',')
+#  print(ind_data[0])
+  subprocess.call("./insert_date.sh", shell=True)
 
-# daily_activity() determines whether there has been any trading activity during the current day.  It compares the current day's total volume against the previous day's total volume. If the two are different, it assumes that there was market activity and runs the rest of the program.  If the two are the same, it assumes that the market's were not open today and exits the program
-def daily_activity():
-# Using pandas dataframes, this sections defines the columns widths from the from the scrape_industries function
-  path = "industries_scraped.txt"
-  col_specification =[(0, 5), (6, 13), (14, 33), (34, 51), (52, 60), (61, 69)]
-  current = pd.read_fwf(path, width=col_specification)
-  current_index = current.set_index("Symbol")
-# Using pandas dataframes, this sections defines the columns widths from the from the scrape_industries function for the previous day
-  path = "industries_previous.txt"
-  col_specification =[(0, 5), (6, 13), (14, 33), (34, 51), (52, 60), (61, 69)]
-  previous = pd.read_fwf(path, width=col_specification)
-  previous_index = previous.set_index("Symbol")
+# Determine if there has been any trading activity by comparing the current day's total volume
+# against the previous day's total volume. If the two are different, it assumes that there was
+# market activity and runs the rest of the program.
+  ind_price = pd.read_csv("./tmp/ind_price")
+#  current_index = ind_price.set_index("Symbol")
 
-  if float(current_index.loc["^TV.US"]["Volume"]) == float(previous_index.loc["^TV.US"]["Volume"]):
-    industry = "test"
-    logging.basicConfig(format='%(asctime)s %(message)s', filename='logfile.txt',level=logging.DEBUG)
+  previous = pd.read_csv("./tmp/ind_price_prev")
+#  previous_index = previous.set_index("Symbol")
+  if ind_price.iloc[0]['Price'] == previous.iloc[0]['Price']:
+    print("No activity detected for", print (now))
+    logging.basicConfig(format='%(asctime)s %(message)s',filename='logfile.txt',level=logging.DEBUG)
     logging.warning("No trading activity detected today")
-    print("No activity detected for ", today)
-    print ("{:,d}".format(current_index.loc["^TV.US"]["Volume"]), "{:,d}".format(previous_index.loc["^TV.US"]["Volume"]))
+
+
+# At this point, you will need to write the date to an html page
+    print ("{:,d}".format(current_index.loc["^TV.US"]["Price(Intraday)"]),"{:,d}".format(previous_index.loc["^TV.US"]["Price(Intraday)"]))
     return "no"
+
   else:
-    print("Activity detected for ", datetime.date.today().strftime("%B"), datetime.date.today().strftime("%d"), datetime.date.today().strftime("%Y"))
-    # This next line overwrites the previous volume with the latest volume copies the industries_current.txt file to the industries_previous.txt file
-    with open('industries_previous.txt', 'w+') as output, open('industries_current.txt', 'r') as input:
-      output.write(input.read())
+    print("Activity detected for", datetime.date.today().strftime("%B"), datetime.date.today().strftime("%d"), datetime.date.today().strftime("%Y"))
+    print ("Difference ", ind_price.iloc[0]['Price'], " ", previous.iloc[0]['Price'])
+# Remove the Total and Wholesale records from the current day's activity
+    ind_price = ind_price[ind_price.Symbol != '^YHOH860']
+    ind_price = ind_price[ind_price.Symbol != '^TV.US']
+    ind_price.to_csv('./tmp/ind_price2', header=None, index=False, sep=',')
+
+# Copy the ind_price.csv table to the ind_price postgres table
+    db_string = "postgres://postgres:password@localhost:5432/postgres"
+    conn = psycopg2.connect(db_string)
+    cur = conn.cursor()
+    with open('./tmp/ind_price2', "r") as infile:
+      next (infile) # Skip the row header
+      cur.copy_from(infile, 'ind_price', sep=',')
+      conn.commit()
+      cur.close()
     return "yes"
+    infile.close()
 
 # This part of the program creates and formats urls for each industry
-def format_industry_urls():
-  from random import randint
-  from time import sleep
-  sleep(randint(10,20))
-  sauce = urllib.request.urlopen('https://finance.yahoo.com/industries').read()
-  soup = bs.BeautifulSoup(sauce,'lxml')
-  f = open('href_scraped.txt', 'w')
-  for url in soup.find_all('a'):
-    print(url, end="", file=f, flush=False)
-  f.close()
+#def format_industry_urls():
+#  from random import randint
+#  from time import sleep
+#  sleep(randint(10,20))
+#  sauce = urllib.request.urlopen('https://finance.yahoo.com/industries').read()
+#  soup = bs.BeautifulSoup(sauce,'lxml')
+#  f = open('./tmp/ind_urls', 'w')
+#  for url in soup.find_all('a'):
+#    print(url, end="", file=f, flush=False)
+#  f.close()
 # subprocess.call formats the hrefs to yahoo finance industry urls
-  subprocess.call("./href_edit.sh", shell=True)
+#  subprocess.call("./href_edit.sh", shell=True)
 
 # Read the yahoo finance industry urls into an array and for each array, run the volume comparisons on both the specific industry and with each stock
 def get_stocks():
-  with open('industry_urls.txt') as f:
-      content = f.readlines()
-      for p in content:
+  with open('./tmp/url_data2', "r") as infile:
+      content = infile.readlines()
+# Skip reading the first line since it contains /industry/Wholesale_Other which is not an Industry
+#      next(content, None)
+      ind_df = pd.read_csv('./tmp/url_data2')
+      url = ind_df.set_index("URL", drop = False)
+      print (ind_df.columns)
+
+#      for s in range (0, len(url.index)):
+#        print (ind_df['URL'])
+#        print (ind_df.iloc[0]['url'])
+#ind_price.iloc[0]['Price']
+      for s in range (0, len(ind_df.index)):
+        url_link = ind_df.get_value(s, 'URL')
+        ind_symbol = ind_df.iloc[s]['Symbol']
+        print ('url_link ', url_link, 'Symbol ', ind_symbol)
         from random import randint
         from time import sleep
         sleep(randint(10,20))
-        lhs, industry = p.split("industry/", 1)
-        txt = ".txt"
-        industry = industry.rstrip() + txt
+
         try:
-           urllib.request.urlopen(p)
+           urllib.request.urlopen(url_link)
         except urllib.error.HTTPError as err:
-           logging.basicConfig(format='%(asctime)s %(message)s', filename='logfile.txt',level=logging.DEBUG)
-           logging.warning("Invalid URL for '{0}'".format(industry))
+           logging.basicConfig(format='%(asctime)s %(message)s',filename='logfile',level=logging.DEBUG)
+           logging.warning("Invalid URL for '{0}'".format(url_link))
         else:
-           f = open(industry, 'w')
+           stock_data = open('./tmp/stock_data', 'w')
         try:
-           ind_df = pd.read_html(p,header=0)
-        except Exception as e: 
-           logging.basicConfig(format='%(asctime)s %(message)s', filename='logfile.txt',level=logging.DEBUG)
-           logging.warning("There are no stocks in '{0}'".format(industry))
+#           ind_df = pd.read_html(p,header=0)
+           stock_data = pd.read_html(url_link, header=0)
+#           ind_df = pd.read_csv('./tmp/url_data')
+        except Exception as e:
+           logging.basicConfig(format='%(asctime)s %(message)s',filename='logfile',level=logging.DEBUG)
+           logging.warning("There are no stocks in '{0}'".format(url_link))
         else:
-           pd.set_option('display.max_rows', 500, 'display.width', 1000)
-           print(ind_df, end="", file=f, flush=False)
-           f.close()
-	   # The following subprocess.call functions normalize the column headers
-           subprocess.call(["sed", "-i", "-e",  's/\[//g', industry])
-           subprocess.call(["sed", "-i", "-e",  's/Price (Intraday)/           Price/g', industry])
-           subprocess.call(["sed", "-i", "-e",  's/Avg Vol (3 month)/           AvgVol/g', industry])
-           subprocess.call(["sed", "-i", "-e",  's/% Change/ %Change/g', industry])
-           subprocess.call(["sed", "-i", "-e",  's/Market Cap/ MarketCap/g', industry])
-           subprocess.call(["sed", "-i", "-e",  's/PE Ratio (TTM)/       PERatio/g', industry])
-           subprocess.call(["sed", "-i", "-e",  's/52 Week Range/  52WeekRange/g', industry])
-           subprocess.call(["awk", "-v", "x=tmp.txt", '{printf "%6s %8s %10s %10s %10s %12s \\n", $2, $(NF-7), $(NF-4), $(NF-3), $(NF-2), $(NF) > x;}', industry ])
-           from shutil import copyfile
-           copyfile("tmp.txt", industry)        
-           statinfo = os.stat(industry)
+#           print('stock_data', stock_data[0].columns)
+#           stock_data[0].to_csv('./tmp/stock_data', header=None, index=False, sep=',')
+#           print (ind_df.columns)
 
-	   # The if statement checks to see if the list of stocks in an industry is zero to prevent an error when reading the file into a dataframe
-           if statinfo.st_size == 0:
-             print (industry, " file size is 0 bytes.")
-           else:
-             from random import randint
-             from time import sleep
-             sleep(randint(10,20))
-             col_specification =[(0, 5), (6, 14), (15, 26), (27, 37), (38, 48), (49, 61)]
-             data = pd.read_fwf(industry, width=col_specification)
+# Yahoo Finance displays volumes using commas and M's (to denote millions).  This section normalizes the data to numeric values
+           ind_vol = 0
+           avg_ind_vol = 0
+           for i in range (0, len(stock_data[0].index)):
+             print (stock_data[0].iloc[i]['Volume'])
+             if "M" in str(stock_data[0].iloc[i]['Volume']):
+#               print (stock_data[0].iloc[i]['Volume'])
+               tempVol = int(float(str(stock_data[0].iloc[i]['Volume']).replace("M", "")) * 1000000)
+               ind_vol = int(ind_vol) + int(tempVol);
+#               stock_data[0].loc[i, 'Volume'] = tempVol
+#               stock_data[0].loc[i, 'Volume'] = int(stock_data[0].loc[i, 'Volume'])
+#               print ('Updated volume ', stock_data[0].iloc[i]['Volume'])
 
-           # Yahoo Finance displays volumes using commas and M's (to denote millions).  This section normalizes the data to numeric values
-             for i in range (0, len(data.index)):
-               if "M" in str(data.iloc[i, data.columns.get_loc('Volume')]):      
-                 tempVol = str(data.iloc[i, data.columns.get_loc('Volume')]).replace("M", "")
-                 tempVol = float(tempVol) * 1000000
-                 tempVol = int(tempVol)
-                 data.at[i, 'Volume'] = tempVol
-               else:
-                 tempVol = int(data.iloc[i, data.columns.get_loc('Volume')])
-                 data.at[i, 'Volume'] = tempVol
-               if (pd.isnull(data.iloc[i, data.columns.get_loc('AvgVol')])):
-                 data.at[i, 'AvgVol'] = data.at[i, 'Volume']
-               elif "M" in str(data.iloc[i, data.columns.get_loc('AvgVol')]):  
-                 tempVol = str(data.iloc[i, data.columns.get_loc('AvgVol')]).replace("M", "")
-                 tempVol = float(tempVol) * 1000000
-                 tempVol = int(tempVol)
-                 data.at[i, 'AvgVol'] = tempVol
-               else:
-                 tempVol = int(data.iloc[i, data.columns.get_loc('AvgVol')])
-                 data.at[i, 'AvgVol'] = tempVol
 
-             totalVol = data['Volume'].sum()
-             totalAvgVol = data['AvgVol'].sum()
-             if (totalVol) >= (totalAvgVol*2):
-               increase_in_volume = float(totalVol)/float(totalAvgVol)
-               final = open('increase_in_volume.txt', 'a')
-               print("The volume for ", industry, " was ", increase_in_volume, " times higher than the 30-day average.")
-               final.close()
+# Checkout https://stackoverflow.com/questions/13842088/set-value-for-particular-cell-in-pandas-dataframe-using-index
              else:
-               print (industry, "Total volume is within the normal range",  "Total Volume ={:,d}".format(totalVol), "  Total Average Volume = {:,d}".format(totalAvgVol))
+               ind_vol = (stock_data[0].iloc[i]['Volume'])
+             if "M" in str(stock_data[0].iloc[i]['Avg Vol (3 month)']):
+               print (stock_data[0].iloc[i]['Avg Vol (3 month)'])
+               tempVol = int(float(str(stock_data[0].iloc[i]['Avg Vol (3 month)']).replace("M", "")) * 1000000)
+               stock_data[0].loc[i, 'Avg Vol (3 month)'] = tempVol
+               stock_data[0].loc[i, 'Avg Vol (3 month)'] = int(stock_data[0].loc[i, 'Avg Vol (3 month)'])
+               print ('tempVol ', tempVol, 'Updated ', (stock_data[0].iloc[i]['Avg Vol (3 month)']))
+               avg_ind_vol = int(avg_ind_vol) + int(tempVol);
+             else:
+               avg_ind_vol = (stock_data[0].iloc[i]['Avg Vol (3 month)'])
+# At this point you have the values to write directly to the database, I think
+           print ('ind_vol ', ind_vol, 'avg_ind_vol ', avg_ind_vol)
+           percent_change_volume = int(ind_vol)/int(avg_ind_vol)*100
+
+           db_string = "postgres://postgres:password@localhost:5432/postgres"
+           conn = psycopg2.connect(db_string)
+           cur = conn.cursor()
+           cur.execute("INSERT INTO ind_vol(ind_symbol, trade_date, ind_vol, avg_ind_vol, percent_change_volume) VALUES (%s, %s, %s, %s, %s)", (ind_symbol, now, int(ind_vol), int(avg_ind_vol), percent_change_volume))
+           conn.commit()
+           cur.close
+
+
+
+#           print ('percent_change_volume ', percent_change_volume)
+#           print ('ind_symbol ', ind_symbol)
+#           print ('trade_date ', now)
+#           print ('ind_vol ', ind_vol)
+#           print ('avg_ind_vol ', avg_ind_vol)
+#           print ('percent_change_volume', percent_change_volume)
+
+#           print ('percent_change_volume ', type(percent_change_volume))
+#           print ('ind_symbol ', type(ind_symbol))
+#           print ('trade_date ', type(now))
+#           print ('ind_vol ', type(ind_vol))
+#           print ('avg_ind_vol ', type(avg_ind_vol))
+#           print ('percent_change_volume', type(percent_change_volume))
+#           print ('ind_symbol ', ind_symbol, 'trade_date ', now, 'ind_vol ', ind_vol, 'avg_ind_vol ', avg_ind_vol, 'percent_change_volume', percent_change_volume);
+
+#             totalAvgVol = stock_data[0]['Avg Vol (3 month)'].sum()
+#             stock_data[0].to_csv('./tmp/stock_data', header=None, index=False, sep=',')
+#             if (totalVol) >= (totalAvgVol*2):
+#               increase_in_volume = float(totalVol)/float(totalAvgVol)
+#               final = open('increase_in_volume.txt', 'a')
+#               print("The volume for ", industry, " was ",increase_in_volume, " times higher than the 30-day average.")
+#             stock_data.close()
+#             else:
+#               print (industry, "Total volume is within the normal range",  "Total Volume ={:,d}".format(totalVol), "  Total Average Volume = {:,d}".format(totalAvgVol))
+  infile.close()
+#  stock_data.close()
 
 if __name__ == '__main__':
-   scrape_industries()
-   if daily_activity() == "no":
-       print("daily_activity = no")
-   else:
-       print("daily_activity = yes")
-       format_industry_urls()
-       get_stocks()
+   now = datetime.date.today()
+#   scrape_industries()
+#   if daily_activity() == "no":
+#       print("daily_activity = no")
+#   else:
+#       print("daily_activity = yes")
+#       format_industry_urls()
+   get_stocks()
+
